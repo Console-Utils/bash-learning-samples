cd5122ce60bf89176349c6bf82c439ebb140594a
Alvin Seville <alvinseville7cf@gmail.com>
2020-09-13 10:33:25 +0000

    Random Text Foreground Coloring.sh

---
+++ Random Text Foreground Coloring.sh
@@ -0,0 +1,12 @@
+declare -a colors=({30..37} {90..97} 39)
+declare -i colorsLength=${#colors[@]}
+
+text="Hello World!"
+textLength=${#text}
+
+for i in $(seq 0 $(($textLength - 1)))
+do
+  declare -i j=$((RANDOM % colorsLength))
+  echo -e -n "\e[${colors[j]}m${text:i:1}\e[0m"
+done
+echo
